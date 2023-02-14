cf3d786c32ed42787d4ed923410a7c9524253f20
Michael Smith <smithmi@sonoma.edu>
2023-02-14 11:59:02 -0800

    Updated hardcoded numbers into Variables

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ Description: This game is called shape destroyer and the objective is to destroy
 running out of bullets. The User moves around and shoots bullets at the enemy shapes and needs to hit them in order
 to destroy the enemy shape. The User is an independent object, but the Enemy Shapes and Bullets are objects inherited
 from the parent class, "GameObject".
- */
+*/
 
 
 #include <SFML/Graphics.hpp>
@@ -75,17 +75,20 @@ public:
     bool bulletDown = false;
     bool bulletLeft = false;
     bool bulletRight = false;
+    int width = 10;
+    int height = 10;
 
+    // Create bullet object and specialize attributes
     static Bullet create_Bullet() {
         Bullet bullet;
-        bullet.attr.setSize(sf::Vector2f(10, 10));
+        bullet.attr.setSize(sf::Vector2f(bullet.width, bullet.height));
         bullet.attr.setFillColor(sf::Color::Red);
-//        cout << "Printing player position X: " << player.getPosition().x << " & y: " <<  player.getPosition().y << endl;
 
         cout << "Bullet Created" << endl;
         return bullet; // Returning an object (bullet)
     }
 
+    // Loading the queue with a fixed number of bullets
     static queue<Bullet> loadBulletQueue(queue<Bullet> & bulletQueue) {
         for ( int i = 0; i < NUM_BULLETS; i++ ){
             Bullet bullet = Bullet::create_Bullet();
@@ -94,10 +97,10 @@ public:
         return bulletQueue;
     }
 
-    static void fireBullet(queue<Bullet> & bulletQueue, vector<Bullet> & bulletVector){
-
+    // Take ojecct from bulletQueue and add it to activeBulletVect since bullet has been fired - pop a bullet from bulletQueue
+    static void fireBullet(queue<Bullet> & bulletQueue, vector<Bullet> & activeBulletVect){
         bulletQueue.front().isActive = true;
-        bulletVector.push_back(bulletQueue.front());
+        activeBulletVect.push_back(bulletQueue.front());
         bulletQueue.pop();
     }
 
@@ -136,7 +139,7 @@ bool EnemyShapeBulletCollision( sf::Vector2f enemyShapePosition,  sf::Vector2f b
     return false;
 }
 
-sf::Text setTextCharacteristics(sf::Text & textObject, int xPos, int yPos, int charSize) {
+sf::Text setTextCharacteristics(sf::Text & textObject, int xPos, int yPos, int charSize){
     textObject.setPosition(xPos, yPos);
     textObject.setCharacterSize(charSize);
     textObject.setStyle(sf::Text::Bold);
@@ -176,17 +179,19 @@ Bullet checkBulletBoundaries(Bullet bullet){
 
 int main() {
 
-    srand(time(NULL));
+    srand(time(nullptr));
 
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Shape Destroyers");
     window.setFramerateLimit(60);
 
     // CREATE USER SHAPE
     sf::CircleShape player;
-    player.setRadius(20);
+    player.setRadius(20); // 20 pixels
     player.setFillColor(sf::Color::Green);
     player.setOrigin(1, 1);
     player.setPosition(WIDTH / 2, HEIGHT / 1.2);
+    float xOffset = 14.5;
+    float yOffset = 15;
 
     // INITIALIZE VARIABLES
     bool keyPressed = false;
@@ -195,8 +200,8 @@ int main() {
     bool outOfBullets = false;
     bool enemyShapeVectEmpty = false;
     int score = 0;
-
     vector<int> WASDvect = {0, 0, 0, 0};
+
 
     // CREATE ENEMY SHAPES AND ADD THEM TO A VECTOR
     vector<EnemyShape> enemyShapeVect;
@@ -206,9 +211,16 @@ int main() {
     queue<Bullet> bulletQueue;
     Bullet::loadBulletQueue(bulletQueue);
 
+    // CREATE ACTIVE BULLET VECT
+    vector<Bullet> activeBulletVect;
+
+
     // DECLARE AND LOAD FONT
     sf::Font font;
-    if ( !font.loadFromFile("arial.ttf") ) exit(1); // error...
+    if ( !font.loadFromFile("arial.ttf") ){
+        cout << "Couldn't Load Font" << endl;
+        exit(1); // error...
+    }
 
     // INITIATE AND SPECIALIZE TEXTS
     //     Score Text
@@ -237,13 +249,6 @@ int main() {
     BeginBuffer.setFillColor(sf::Color::Blue);
 
 
-    // Creates bullets and adds them to the bulletVect
-    vector<Bullet> bulletVect;
-    for ( int i = 0; i < NUM_BULLETS; i++ ){
-        Bullet bullet = Bullet::create_Bullet();
-        bulletVect.push_back(bullet);
-    }
-
 
     while ( window.isOpen() ){
 
@@ -257,12 +262,12 @@ int main() {
         if (enemyShapeVect.empty() ){
             enemyShapeVectEmpty = true;
         }
-        if (  bulletQueue.empty() &&  bulletVect.empty() ){ // TODO: Fix this
+        if (  bulletQueue.empty() &&  activeBulletVect.empty() ){
             outOfBullets = true;
             gameOver = true;
         }
 
-        // Get position of enemy shape and user
+        // ACCESS THE USER'S POSITION
         sf::Vector2f playerPosition = player.getPosition();
 
         if ( sf::Keyboard::isKeyPressed(sf::Keyboard::Escape) ) // Press Escape to Close Window
@@ -294,64 +299,54 @@ int main() {
         if (event.type == sf::Event::KeyReleased){
             if ( WASDvect[0] ) {  // Shoot bullet Upwards
                 keyPressed = true;
-
-                bulletQueue.front().attr.setPosition(playerPosition.x + 14.5, playerPosition.y);
-                bulletQueue.front().bulletUp = true;
-                cout << "Adding bulletQueue.front() to bulletVect for W pressed" << endl;
-
                 WASDvect[0] = 0;
-//                W = false;
-
+                // Set bullet Object attributes
+                bulletQueue.front().attr.setPosition(playerPosition.x + xOffset, playerPosition.y);
+                bulletQueue.front().bulletUp = true;
             }
             else if ( WASDvect[1] ) { // Shoot bullet Left
                 keyPressed = true;
-
-                bulletQueue.front().attr.setPosition(playerPosition.x, playerPosition.y + 15);
-                bulletQueue.front().bulletLeft = true;
-                cout << "Adding bulletQueue.front() to bulletVect for A pressed" << endl;
                 WASDvect[1] = 0;
+                // Set bullet Object attributes
+                bulletQueue.front().attr.setPosition(playerPosition.x, playerPosition.y + yOffset);
+                bulletQueue.front().bulletLeft = true;
             }
             else if ( WASDvect[2] ) { // Shoot bullet Down
                 keyPressed = true;
-
-                bulletQueue.front().attr.setPosition(playerPosition.x + 14.5, playerPosition.y + 29);
-                bulletQueue.front().bulletDown = true;
-                cout << "Adding bulletQueue.front() to bulletVect for S pressed" << endl;
                 WASDvect[2] = 0;
+                // Set bullet Object attributes
+                bulletQueue.front().attr.setPosition(playerPosition.x + xOffset, playerPosition.y + (2 * yOffset));
+                bulletQueue.front().bulletDown = true;
             }
             else if ( WASDvect[3] ) { // Shoot bullet Right
                 keyPressed = true;
-
-                bulletQueue.front().attr.setPosition(playerPosition.x + 29, playerPosition.y + 15);
-                bulletQueue.front().bulletRight = true;
-                cout << "Adding bulletQueue.front() to bulletVect for D pressed" << endl;
                 WASDvect[3] = 0;
+                // Set bullet Object attributes
+                bulletQueue.front().attr.setPosition(playerPosition.x + (2 * xOffset), playerPosition.y + yOffset);
+                bulletQueue.front().bulletRight = true;
             }
         }
 
+        // ONLY MOVE BULLET OBJECT FROM QUEUE TO ACTIVE BULLET VECT IF THERE IS BULLETS LEFT IN THE QUEUE
+        if (!bulletQueue.empty() && keyPressed) Bullet::fireBullet(bulletQueue, activeBulletVect);
 
-        if (!bulletQueue.empty() && keyPressed) Bullet::fireBullet(bulletQueue, bulletVect);
-
-        // MOVING BULLET
-        for (int j = 0; j < bulletVect.size(); j++) {
-            int XBulletpos = bulletVect[j].attr.getPosition().x;
-            int YBulletpos = bulletVect[j].attr.getPosition().y;
-
-            if (bulletVect[j].bulletUp){
-                bulletVect[j] = Bullet::moveBullet(bulletVect[j], 0, -3, 1, 1);
+        // MOVING EACH ACTIVE BULLET IN NECESSARY DIRECTION
+        for (int j = 0; j < activeBulletVect.size(); j++) {
+            if (activeBulletVect[j].bulletUp){
+                activeBulletVect[j] = Bullet::moveBullet(activeBulletVect[j], 0, -3, 1, 1);
             }
-            else if(bulletVect[j].bulletLeft){
-                bulletVect[j] = Bullet::moveBullet(bulletVect[j], 3, 0, -1, 1);
+            else if(activeBulletVect[j].bulletLeft){
+                activeBulletVect[j] = Bullet::moveBullet(activeBulletVect[j], 3, 0, -1, 1);
             }
-            else if(bulletVect[j].bulletDown){
-                bulletVect[j] = Bullet::moveBullet(bulletVect[j], 0, 3, 1, 1);
+            else if(activeBulletVect[j].bulletDown){
+                activeBulletVect[j] = Bullet::moveBullet(activeBulletVect[j], 0, 3, 1, 1);
             }
-            else if(bulletVect[j].bulletRight){
-                bulletVect[j] = Bullet::moveBullet(bulletVect[j], 3, 0, 1, 1);
+            else if(activeBulletVect[j].bulletRight){
+                activeBulletVect[j] = Bullet::moveBullet(activeBulletVect[j], 3, 0, 1, 1);
             }
 
             // Delete bullet if it goes out of the boundaries
-            bulletVect[j] = checkBulletBoundaries(bulletVect[j]);
+            activeBulletVect[j] = checkBulletBoundaries(activeBulletVect[j]);
         }
 
 
@@ -367,14 +362,13 @@ int main() {
             }
 
             // Traverse through each active bullet for collisions with any objects
-            for (int j = 0; j < bulletVect.size(); j++){
-                sf::Vector2f bulletPosition = bulletVect[j].attr.getPosition();
+            for (int j = 0; j < activeBulletVect.size(); j++){
+                sf::Vector2f bulletPosition = activeBulletVect[j].attr.getPosition();
 
                 // Check if position of enemy shape and a bullet hit each other
-                if ( EnemyShapeBulletCollision(enemyShapePosition, bulletPosition) && bulletVect[j].isActive ){
-//                cout << "Enemy Shape Collision...enemy object # = " << enemyShapeVect[i].enemyShapeNum << endl;
+                if ( EnemyShapeBulletCollision(enemyShapePosition, bulletPosition) && activeBulletVect[j].isActive ){
                     enemyShapeVect[i].isActive = false;
-                    bulletVect[j].isActive = false;
+                    activeBulletVect[j].isActive = false;
                     score += 1;  // Increment Score
                 }
             }
@@ -401,10 +395,10 @@ int main() {
 
 
         // DELETE NON ACTIVE BULLETS IN BULLET VECT
-        for (int i = 0; i < bulletVect.size(); i++ ){
-            if(!bulletVect[i].isActive){
-                swap(bulletVect[i], bulletVect.back());
-                bulletVect.pop_back();
+        for (int i = 0; i < activeBulletVect.size(); i++ ){
+            if(!activeBulletVect[i].isActive){
+                swap(activeBulletVect[i], activeBulletVect.back());
+                activeBulletVect.pop_back();
             }
         }
 
@@ -459,9 +453,9 @@ int main() {
             for ( int i = 0; i < NUM_ENEMY_SHAPES; i++ ){
                 window.draw(enemyShapeVect[i].attr);
             }
-            for (int j = 0; j < bulletVect.size(); j++){
-                if ( bulletVect[j].isActive ){
-                    window.draw(bulletVect[j].attr);
+            for (int j = 0; j < activeBulletVect.size(); j++){
+                if ( activeBulletVect[j].isActive ){
+                    window.draw(activeBulletVect[j].attr);
                 }
             }
             window.display();
@@ -473,10 +467,6 @@ int main() {
 
 
 // TODO:
-//  - sometimes enemy shapes move horizontally across side of screen
-//      - I think it's bc rand sometimes is 0
-//  - Fix enemy shapes spawning near user at the beginning
-//  - Fix all fixed numbers and put them as variables
 //  - Delete Enemy Shape Object Num before I send it
 //  - Maybe don't use pass by reference - is it harder to delete objects/memory?
 //  - Is it possible to make anywhere more efficient? Better time complexity?
